9b62099c4d9b04ada47f49bd8961a424f8b5bbc9
Rigil-Kent <18565214+tsubashii@users.noreply.github.com>
2019-09-09 15:24:10 +0100

    implemented modal on movie and tvshow

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import '../App.css';
 import { imgURL, myApiKey, moviePopular } from './api/APIUtils';
+import Modal from 'react-responsive-modal';
 
 class Movie extends React.Component {
     _isMounted = false; // to prevent no-op/ memory leak -- class field that holds the lifecycle state of your component, to prevent this.setState() being called     
@@ -9,7 +10,9 @@ class Movie extends React.Component {
         super(props);
         
         this.state = {
-            movies : []
+            movies : [],
+            open : false,
+            selectedMovie : null // keep track of selected Item
         }
     }
     componentDidMount() {
@@ -31,12 +34,52 @@ class Movie extends React.Component {
         this._isMounted = false;
     }
 
+    onOpenModal = i => {
+        this.setState({ 
+            open: true,
+            selectedMovie: i // when Item is clicked, mark as selected
+         });
+    }
+
+    onCloseModal = () => {
+        this.setState({ open: false });
+    }
+
+    renderModal = () => {
+        // check to see if there's a selected item/post/movie. If so, render it.
+        if (this.state.selectedMovie !== null) {
+            const movie = this.state.movies[this.state.selectedMovie];
+            return (
+                <div style={{ width: 500, height: 630, backgroundColor: "white" }}>
+                    <img src={ imgURL + movie.poster_path } 
+                        style={{ width: 150, height: 230 }} alt='movie poster'>
+                    </img>
+                    <h5>Name: {movie.original_title}</h5>
+                    <p>Release Date: { movie.release_date }</p>
+                    <p>Vote Average: { movie.vote_average }</p>
+                    <p>Rating: { movie.popularity }</p>
+                    <p>Vote Average: { movie.vote_average }</p>
+                    <p>Vote Count: { movie.vote_count }</p>
+                    <p>Overview: { movie.overview }</p>
+                </div>
+            );
+        }
+    }
+
     render() {
+        const { open } = this.state;
         // iterate over movies and display in a grid
-        const movies= this.state.movies.map((movie, index)=> {
+        const movies = this.state.movies.map((movie, index)=> {
             return(
                 <div className="movie-card col s4 m6 l3" key={ index }>
-                    <a href="#!"><img src={ imgURL + movie.poster_path } alt='movie poster'></img></a>
+                    <a href="#!" onClick={() => this.onOpenModal(index)}>
+                        <img src={ imgURL + movie.poster_path } 
+                            alt='movie poster'>
+                        </img>
+                    </a>
+                    <Modal open={open} onClose={this.onCloseModal} animationDuration={500} center>
+                        <div>{this.renderModal()}</div>
+                    </Modal>
                 </div>
             )
         });
--- src/components/TvShow.js
+++ src/components/TvShow.js
@@ -2,15 +2,18 @@ import React from 'react';
 import axios from 'axios';
 import '../App.css';
 import { imgURL, myApiKey, tvshowPopular } from './api/APIUtils';
+import Modal from 'react-responsive-modal';
 
 class Tvshow extends React.Component {
     _isMounted = false; // to prevent no-op/ memory leak -- class field that holds the lifecycle state of your component, to prevent this.setState() being called         
 
     constructor(props) {
         super(props);
-        
+
         this.state = {
-            shows : []
+            shows : [],
+            open: false,
+            selectedShow: null // keep track of selected Item
         }
     }
 
@@ -33,12 +36,50 @@ class Tvshow extends React.Component {
         this._isMounted = false;
     }    
 
+    onOpenModal = i => {
+        this.setState({ 
+            open: true,
+            selectedShow: i // when Item is clicked, mark as selected
+        });
+    }
+
+    onCloseModal = () => {
+        this.setState({ open: false });
+    }
+
+    renderModal = () => {
+        // check to see if there's a selected item/post/show. If so, render it.
+        if (this.state.selectedShow !== null) {
+            const show = this.state.shows[this.state.selectedShow];
+            return (
+                <div style={{ width: 400, height: 450, backgroundColor: "white" }}>
+                    <img src={ imgURL + show.poster_path } 
+                        style={{ width: 150, height: 230 }} alt='show poster'>
+                    </img>
+                    <h5>Name: {show.name}</h5>
+                    <p>First Air Date: { show.first_air_date }</p>
+                    <p>Original language: { show.original_language }</p>
+                    <p>Vote Average: { show.vote_average }</p>
+                    <p>Rating: { show.popularity }</p>
+                </div>
+            );
+        }
+    }
+
     render() {
+        const { open } = this.state;
         // iterate over shows and display in a grid
         const shows= this.state.shows.map((show, index)=> {
             return(
                 <div className="show-card col s4 m6 l3" key={ index }>
-                    <a href="#!"><img src={ imgURL + show.poster_path } alt='tv show poster'></img></a>
+                    <a href="#!" onClick={() => this.onOpenModal(index)}>
+                        <img src={ imgURL + show.poster_path } 
+                            alt='tv show poster'>
+                        </img>
+                    </a>
+                    <Modal open={open} onClose={this.onCloseModal} animationDuration={500} center>
+                        <div>{this.renderModal()}</div>
+                    </Modal>
                 </div>
             )
         });
